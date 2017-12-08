21c1b87f42ca40ed3de7d0e1b7a87e2917c338c0
dkwazhere <dhealth77@gmail.com>
2017-12-08 02:35:10 -0800

    created loop, user click compared to randomly chosen array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ var chooseRandom = movies[random];
 var underScore = [];
 var correctLetter = [];
 var wrongLetter = [];
-// console.log(chooseRandom);
+console.log(chooseRandom);
 
 //create underscores for words in array
 for (i = 0; i < chooseRandom.length; i++) {
@@ -20,12 +20,22 @@ console.log(underScore);
 
 // this loads the underscore array word in the html
 window.onload = function loadit() {
-document.getElementById("randomarray").innerHTML = underScore;
-}
+ document.getElementById("randomarray").innerHTML = underScore;
+ }
 
 //user click
 document.onkeyup = function(event) {
     var userClick = event.key;
-    console.log(userClick);
+    // console.log(userClick);
+
+// create a loop, if user clicks a letter and is within the randomly chosen array, fill in the underscore with letter.
+    for (var j = 0; j < chooseRandom.length; j++) {
+        if (chooseRandom[j] === userClick) {
+            underScore[j] = userClick;
+            console.log(underScore);
+            document.getElementById("randomarray").innerHTML = underScore;
+        }
+    }
 }
+
 
