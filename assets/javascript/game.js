560481f51bec46127aaf0ac47a6e404ba2fed20b
dkwazhere <dhealth77@gmail.com>
2017-12-12 21:02:51 -0800

    added pictures to each negative life

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,7 +13,7 @@ var lives = 10;
 //split testing
 var split = chooseRandom.split('');
 console.log(chooseRandom);
-
+//images
 //create underscores for words in array
 // for (i = 0; i < chooseRandom.length; i++) {
 //     underScore.push(" _ ");
@@ -29,9 +29,8 @@ console.log(underScore);
 
 // this loads the underscore array word in the html
 window.onload = function loadit() {
- document.getElementById("randomarray").innerHTML = underScore;
- }
-
+document.getElementById("randomarray").innerHTML = underScore.join(' ');
+document.getElementById('picture').innerHTML = '<img src="assets/images/hangman1.png" />'
 
 //user click
 document.onkeyup = function(event) {
@@ -44,16 +43,55 @@ document.onkeyup = function(event) {
             if (chooseRandom[j] === userClick) {
                 underScore[j] = userClick;
                 console.log(underScore);
-                document.getElementById("randomarray").innerHTML = underScore;
+                document.getElementById("randomarray").innerHTML = underScore.join(' ');
             }
         }
     } else {
-        wrongLetter.push(userClick[j]);
+        wrongLetter.push(userClick);
         lives--;
         document.getElementById("guesses").innerHTML = wrongLetter.join(' ');
+        // adding pictures
+        if (lives === 10) {
+        document.getElementById('picture')
+            .innerHTML = '<img src="assets/images/hangman1.png" />';
+        }
+        else if (lives === 9) {
+            document.getElementById('picture')
+                .innerHTML = '<img src="assets/images/hangman2.png" />';
+        }
+        else if (lives === 8) {
+            document.getElementById('picture')
+                .innerHTML = '<img src="assets/images/hangman3.png" />';
+        }
+        else if (lives === 7) {
+            document.getElementById('picture')
+                .innerHTML = '<img src="assets/images/hangman4.png" />';
+        }
+        else if (lives === 6) {
+            document.getElementById('picture')
+                .innerHTML = '<img src="assets/images/hangman5.png" />';
+        }
+        else if (lives === 5) {
+            document.getElementById('picture')
+                .innerHTML = '<img src="assets/images/hangman6.png" />';
+        }
+        else if (lives === 4) {
+            document.getElementById('picture')
+                .innerHTML = '<img src="assets/images/hangman7.png" />';
+        }
+        else if (lives === 3) {
+            document.getElementById('picture')
+                .innerHTML = '<img src="assets/images/hangman8.png" />';
+        }
+        else if (lives === 2) {
+            document.getElementById('picture')
+                .innerHTML = '<img src="assets/images/hangman9.png" />';
+        }
+        else if (lives === 1) {
+            document.getElementById('picture')
+                .innerHTML = '<img src="assets/images/hangman10.png" />';
+        }
     }
-
-
         // live counter
         document.getElementById("lives").innerHTML = "You have: " + lives + " lives remaining";
         // alerting the loss
@@ -68,4 +106,5 @@ document.onkeyup = function(event) {
             // reloading page after winning
             location.reload();
         }
+    }
 }
